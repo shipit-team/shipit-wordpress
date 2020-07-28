4be2bd83ed236ee97c6378e3bdbcdceaf35b990c
Zamiz <zamiztobar@gmail.com>
2020-07-28 15:26:23 -0400

    Fix: Order with correct attributes when sent

---
+++ shipit/tags/3.0.5/src/shipit_service/products_collection.php
@@ -0,0 +1,20 @@
+<?php
+  class ProductsCollection {
+    public $collection = array();
+
+    public function __construct($collection) {
+      $this->collection = $collection;
+    }
+
+    function getProducts() {
+      $products = array();
+        foreach ($this->collection as $product) {
+        array_push($products, [new Product($product->id,
+                                           $product->amount,
+                                           $product->description,
+                                           $product->warehouse_id)]);
+      }
+      return $products;
+    }
+  }
+?>

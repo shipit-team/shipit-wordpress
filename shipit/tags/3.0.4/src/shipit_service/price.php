2e8b9fc37173e28ff2eb2afca5a32f465930b403
FranciscoArenas <franciscoexiste@gmail.com>
2020-07-28 12:43:09 -0400

    Fix file version to work under svn

---
+++ shipit/tags/3.0.4/src/shipit_service/price.php
@@ -0,0 +1,46 @@
+<?php
+  class Price {
+    public $total = 0.0;
+    public $price = 0.0;
+    public $cost = 0.0;
+    public $tax = 0.0;
+    public $overcharge = 0.0;
+
+    public function __construct($total, $price, $cost, $tax, $overcharge) {
+      $this->total = $total;
+      $this->price = $price;
+      $this->cost = $cost;
+      $this->tax = $tax;
+      $this->overcharge = $overcharge;
+    }
+
+    function getPrice() {
+      return array(
+        'total' => $this->getTotal(),
+        'tax' => $this->getTax(),
+        'cost' => $this->getCost(),
+        'overcharge' => $this->getOvercharge()
+      );
+    }
+
+    function getTotal() {
+      return $this->total;
+    }
+
+    function getShippingPrice() {
+      return $this->price;
+    }
+
+    function getTax() {
+      return $this->tax;
+    }
+
+    function getCost() {
+      return $this->cost;
+    }
+
+    function getOvercharge() {
+      return $this->overcharge;
+    }
+  }
+?>

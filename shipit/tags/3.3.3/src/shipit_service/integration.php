67badfacca6d34e101154f54cd41cb17d09b689b
multinauta <multinauta@gmail.com>
2021-07-02 11:33:47 -0400

    Bug: fix svn problem

---
+++ shipit/tags/3.3.3/src/shipit_service/integration.php
@@ -0,0 +1,79 @@
+<?php
+  class Integration {
+    public $email = '';
+    public $token = '';
+    public $headers = '';
+    public $base = '';
+    public $bugsnag;
+
+    public function __construct($email, $token) {
+      $this->base = 'http://orders.shipit.cl/v';
+      $this->email = $email;
+      $this->token = $token;
+      $this->bugsnag = new Bugsnag();
+      $this->headers = array(
+        'Content-Type' => 'application/json',
+        'X-Shipit-Email' => $email,
+        'X-Shipit-Access-Token' => $token,
+        'Accept' => 'application/vnd.orders.v1'
+      );
+    }
+
+    function setting() {
+      $client = new HttpClient($this->base . '/integrations/seller/woocommerce', $this->headers);
+      $response = $client->get();
+      $setting = array();
+
+      if (is_wp_error($response)) {
+        echo 'Error al conectar con API.';
+      } else {
+        $setting = json_decode($response['body'])->configuration;
+      }
+      return $setting;
+    }
+
+    function configure($setting = array()) {
+      $client = new HttpClient($this->base . '/integrations/configure', $this->headers);
+      $response = $client->put($setting);
+      $setting = array();
+
+      if (is_wp_error($response)) {
+        echo 'Error al conectar con API.';
+      } else {
+        $setting = json_decode($response['body']);
+      }
+      return $setting;
+    }
+
+    function orders($order = array()) {
+      $client = new HttpClient($this->base . '/orders', $this->headers);
+      $reference = $order['order']['reference'];
+      $response = $client->post($order);
+      $order = array();
+      if (is_wp_error($response)) {
+        echo 'Error al conectar con API.';
+        $this->bugsnag->bugsnagWpLog($response, "orders services", $reference);
+      } else {
+        $order = json_decode($response['body']);
+        $this->bugsnag->bugsnagLog($order, "orders services", $reference);
+      }
+      
+      return $order;
+    }
+
+    function massiveOrders($order = array()) {
+      $client = new HttpClient($this->base . '/orders/massive', $this->headers);
+      $references = $this->bugsnag->getReferencesFromOrdersMassive($order);
+      $response = $client->post($order);
+      $order = array();
+      if (is_wp_error($response)) {
+        echo 'Error al conectar con API.';
+        $this->bugsnag->bugsnagWpLog($response, "orders massive service", $references);
+      } else {
+        $order = json_decode($response['body']);
+        $this->bugsnag->bugsnagLog($order, "orders massive service", $references);
+      }   
+      return $order;
+    }
+  }
+?>
---
+++ shipit/tags/3.3.3/src/shipit_service/seller.php
@@ -0,0 +1,47 @@
+<?php
+  class Seller {
+    public $id = '';
+    public $name = 'woocommerce';
+    public $referring_site = '';
+    public $status = 'draft';
+    public $created_at = '';
+
+    public function __construct($id, $created_at = '', $referring_site = '', $status = '', $name = 'woocommerce') {
+      $this->id = $id;
+      $this->name = $name;
+      $this->referring_site = $referring_site;
+      $this->status = $status;
+      $this->created_at= $created_at;
+    }
+
+    function getSeller() {
+      return array(
+        'id' => $this->getId(),
+        'name' => $this->getName(),
+        'reference_site' => $this->getReferringSite(),
+        'created_at' => $this->getCreatedAt(),
+        'status' => $this->getStatus()
+      );
+    }
+
+    function getId() {
+      return $this->id;
+    }
+
+    function getName() {
+      return $this->name;
+    }
+
+    function getReferringSite() {
+      return $this->referring_site;
+    }
+
+    function getStatus() {
+      return $this->status;
+    }
+
+    function getCreatedAt() {
+      return $this->created_at = wp_date("Y-m-d H:i:s");
+    }
+  }
+?>

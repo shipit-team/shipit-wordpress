4e5767c6e687679a8dfac048e92a18e8cf543041
multinauta <multinauta@gmail.com>
2021-07-06 16:12:10 -0400

    5t9etd Bug: prevent non existent frecuency index

---
+++ shipit/tags/3.3.4/src/shipit_service/company.php
@@ -0,0 +1,27 @@
+<?php
+  class Company {
+    public $id = '';
+    public $name = '';
+    public $platform_version = 2;
+    public $cutting_hours = '';
+
+    public function __construct($id, $name, $platform_version, $cutting_hours) {
+      $this->id = $id;
+      $this->name = $name;
+      $this->platform_version = $platform_version;
+      $this->cutting_hours = $cutting_hours;
+    }
+
+    function get_name() {
+      return $this->name;
+    }
+
+    function get_platform_version() {
+      return $this->platform_version;
+    }
+
+    function get_cutting_hours() {
+      return $this->cutting_hours;
+    }
+  }
+?>
